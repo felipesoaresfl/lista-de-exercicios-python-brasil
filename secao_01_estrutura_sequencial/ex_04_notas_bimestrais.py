031caf474f11aa38ca18e98c732b2f75e377a5df
Felipe Soares <felipe.soares013@outlook.com>
2022-05-30 12:39:02 -0300

    resolvidos exercicios 4, 5 e ex 17 incompleto

--- secao_01_estrutura_sequencial/ex_04_notas_bimestrais.py
+++ secao_01_estrutura_sequencial/ex_04_notas_bimestrais.py
@@ -14,5 +14,9 @@ Faça um Programa que peça as 4 notas bimestrais e mostre a média.
 
 
 def calcular_media():
-    """Escreva aqui em baixo a sua solução"""
-
+    nota1 = int(input('Digite a primeira nota: '))
+    nota2 = int(input('Digite a segunda nota: '))
+    nota3 = int(input('Digite a terceira nota: '))
+    nota4 = int(input('Digite a quarta nota: '))
+    media = (nota1+nota2+nota3+nota4)/4
+    print (f'A média anual é {media}')
