0f3d9ab500d83512752f88bd19a6859d95f87dcb
Felipe Soares <felipe.soares013@outlook.com>
2022-05-31 00:35:01 -0300

    resolvido ex 13 e 14

--- secao_01_estrutura_sequencial/ex_14_joao_papo_de_pescador.py
+++ secao_01_estrutura_sequencial/ex_14_joao_papo_de_pescador.py
@@ -26,3 +26,9 @@ Mostrar o peso e multa com duas casas decimais
 
 def calcular_peso_excedente_e_multa():
     """Escreva aqui em baixo a sua solução"""
+    kg_peixe = float (input('Quantidade de kg de peixes pescados: ')) 
+    excedente = kg_peixe - 50
+    multa = excedente * 4
+
+    print(f'O peso excedente de peixes é de {excedente:.2f} kg')
+    print(f'Por isso, a multa é de R$ {multa:.2f}')
