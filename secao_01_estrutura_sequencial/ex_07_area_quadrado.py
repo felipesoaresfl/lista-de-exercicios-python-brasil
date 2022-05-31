a61a6ed030ee900390d177a3b0292fc8248853db
Felipe Soares <felipe.soares013@outlook.com>
2022-05-31 00:05:32 -0300

    resolvido ex 07

--- secao_01_estrutura_sequencial/ex_07_area_quadrado.py
+++ secao_01_estrutura_sequencial/ex_07_area_quadrado.py
@@ -20,7 +20,7 @@ Mostrar a área com 2 casas decimais.
 
 def calcular_area_de_quadrado():
     """Escreva aqui em baixo a sua solução"""
-    lado = float (input('Digite o lado do quadrado:'))
+    lado = float (input('Digite o lado do quadrado: '))
     area = lado*lado
     dobro= area*2
     print(f'A área do quadrado com esse lado é: {area:.2f}')
