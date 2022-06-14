0f119932630d1d97eca58b0262938c4bebe11745
Felipe Soares <felipe.soares013@outlook.com>
2022-06-14 11:13:50 -0300

    resolvido ex 13 seção 3

--- secao_03_estrutura_de_repeticao/ex_13_potenciacao.py
+++ secao_03_estrutura_de_repeticao/ex_13_potenciacao.py
@@ -18,3 +18,7 @@ Não utilize a função de potência da linguagem.
 
 def calcular_potencia(base: int, expoente: int) -> int:
     """Escreva aqui em baixo a sua solução"""
+    potencia = 1
+    for i in range(expoente):
+        potencia *= base
+    return potencia
