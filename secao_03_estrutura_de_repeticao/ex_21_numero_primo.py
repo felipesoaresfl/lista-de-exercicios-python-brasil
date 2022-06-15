d11d1428cf02098ceab2faaa656d87f6a8f3d872
Felipe Soares <felipe.soares013@outlook.com>
2022-06-15 09:44:35 -0300

    resolvido ex 21 seção 3

--- secao_03_estrutura_de_repeticao/ex_21_numero_primo.py
+++ secao_03_estrutura_de_repeticao/ex_21_numero_primo.py
@@ -39,3 +39,12 @@ divisível somente por ele mesmo e por 1.
 
 def eh_primo(n: int) -> bool:
     """Escreva aqui em baixo a sua solução"""
+    primo = False
+    cont = 0
+
+    for i in range(1, n+1):
+        if n % i == 0:
+            cont += 1
+    if cont == 2:
+        primo = True
+    return primo
