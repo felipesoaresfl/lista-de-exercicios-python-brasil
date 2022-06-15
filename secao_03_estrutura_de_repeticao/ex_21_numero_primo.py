550b2a63646a5b9b11260f4073a6f2ac78dfcfc7
Felipe Soares <felipe.soares013@outlook.com>
2022-06-15 10:05:29 -0300

    resolvido ex 21 seção 3

--- secao_03_estrutura_de_repeticao/ex_21_numero_primo.py
+++ secao_03_estrutura_de_repeticao/ex_21_numero_primo.py
@@ -42,9 +42,9 @@ def eh_primo(n: int) -> bool:
     primo = False
     cont = 0
 
-    for i in range(1, n+1):
+    for i in range(1, n):
         if n % i == 0:
             cont += 1
-    if cont == 2:
+    if cont == 1:
         primo = True
     return primo
